091c6f9ba41bc737117f9ae0559d7e1b9f5f76e2
Anujksingh05 <166947695+Anujksingh05@users.noreply.github.com>
2024-10-20 18:06:49 -1200

    Update java.js

--- java.js
+++ java.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Studen", "Developer",  "Designer", "Freelancer"],
+        strings: ["Student", "Developer",  "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
